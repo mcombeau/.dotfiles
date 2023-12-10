03f7d7bced11ddd49f25f288d999d212a5823e06
mcombeau <mcombeau@student.42.fr>
2023-12-10 18:50:03 +0100

    update shell aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,31 +1,61 @@
 # use colors in coreutils utilities output
 alias ls='ls --color=auto'
 alias grep='grep --color'
-
-# open nvim when vim
-alias vim='nvim'
+alias fgrep='fgrep --color'
+alias egrep='egrep --color'
 
 # protect against overwriting
 alias cp='cp -i'
 alias mv='mv -i'
+alias ln='ln -i'
+alias rm='rm -I --preserve-root'
 
 # ls aliases
 alias ll='ls -la'
+alias lh='ls -lah'
 alias la='ls -a'
 alias l='ls'
+
+# make parent directories aliases
+alias mkdir='mkdir -pv'
 
 # git aliases
 alias gcl='git clone'
 alias gs='git status'
 alias ga='git add'
-alias gc='git commit'
+alias gap='git add -p .'
+alias gc='git commit -m'
 alias gpsh='git push'
 alias gpll='git pull'
+alias grs='git restore --staged .'
 
+# python aliases
 alias py='python3'
 
+# vim aliases
 alias vi='nvim'
+alias vim='nvim'
+alias edit='nvim'
+
 # make directory and cd into it
 mcd() {
 	mkdir "${1}" && cd "${1}"
 }
+
+# apt aliases
+alias update='sudo apt-get update && sudo apt-get upgrade'
+
+# terminal aliases
+alias c='clear'
+alias e='exit'
+alias t='tmux'
+
+# networking
+alias ports='netstat -tulanp'
+
+# reboot/shutdown/poweroff/halt
+alias reboot='sudo /sbin/reboot'
+alias poweroff='sudo /sbin/poweroff'
+alias halt='sudo /sbin/halt'
+alias shutdown='sudo /sbin/shutdown'
+
