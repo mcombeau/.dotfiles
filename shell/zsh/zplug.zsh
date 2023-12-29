5e5f35df23900df6b70c2a3799fc5d0269c8dc9a
mcombeau <mcombeau@student.42.fr>
2023-12-29 18:26:17 +0100

    add zplug config

---
+++ shell/zsh/zplug.zsh
@@ -0,0 +1,14 @@
+source ~/.zplug/init.zsh
+
+zplug "jeffreytse/zsh-vi-mode"
+
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
+
+# Then, source plugins and add commands to $PATH
+zplug load
